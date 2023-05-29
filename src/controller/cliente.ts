a62a08e4d4701b7d6fbbf84ecf5b1ec0ed38aedb
Miguel Carvalho <doxito007@gmail.com>
2023-05-29 20:53:22 -0300

    terminando controller do cliente e petServices

--- src/controller/cliente.ts
+++ src/controller/cliente.ts
@@ -7,7 +7,32 @@ class Cliente {
         const cliente = await clienteServices.get()
         return res.status(200).json(cliente);
     }
-
+    async getPorNome(req: Request, res: Response) {
+        const { nome } = req.params
+        const cliente = await clienteServices.getPorNome(nome)
+        return res.status(200).json(cliente);
+    }
+    async getPorId(req: Request, res: Response) {
+        const { id } = req.params
+        const cliente = await clienteServices.getPorId(parseInt(id))
+        return res.status(200).json(cliente);
+    }
+    async post(req: Request, res: Response) {
+        const { data } = req.body
+        const cliente = await clienteServices.post(data)
+        return res.status(201).json(cliente);
+    }
+    async update(req: Request, res: Response) {
+        const { id } = req.params
+        const { data } = req.body
+        const cliente = await clienteServices.update(parseInt(id), data)
+        return res.status(201).json(cliente);
+    }
+    async delete(req: Request, res: Response) {
+        const { id } = req.params
+        await clienteServices.delete(parseInt(id))
+        return res.status(200)
+    }
 }
 
 export default new Cliente();
--- src/services/clienteServices.ts
+++ src/services/clienteServices.ts
@@ -34,6 +34,42 @@ class ClienteService {
     });
     return cliente;
   }
+
+  async delete(id: number) {
+    await prisma.cliente.delete({
+      where: {
+        id: id
+      }
+    })
+  }
+
+  async update(id: number, data: any) {
+    const cliente = await prisma.cliente.update({
+      data: {
+        nome: data.nome,
+        nomeSocial: data.nomeSocial,
+        dataEmissaoCpf: data.dataEmissao,
+        CPF: data.CPF
+      },
+      where: {
+        id: id
+      }
+    });
+    return cliente
+  }
+
+  async post(data: any) {
+    const cliente = await prisma.cliente.create({
+      data: {
+        nome: data.nome,
+        nomeSocial: data.nomeSocial,
+        dataEmissaoCpf: data.dataEmissao,
+        dataCadastro: new Date(),
+        CPF: data.CPF
+      }
+    });
+    return cliente
+  }
   //
 }
 
